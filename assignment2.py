8b6866116897cf9623c85166290d10edd197c880
sups <supriya1880.cse16@chitkara.edu.in>
2018-08-17 11:28:46 +0530

    assig. 2

---
+++ assignment2.py
@@ -0,0 +1,26 @@
+#question-1
+print("hey there! i'm a student of acadview :) ")
+
+#question-2
+print("chitkara"+" "+"university")
+
+#question-3
+a=int(input("enter a"))
+b=int(input("enter b"))
+c=int(input("enter c"))
+print("value of a:",a,"\nvalue of b:",b,"\nvalue of c:",c)
+
+#question-4
+print("Let's get started")
+
+#question-5
+s="Acadview's"
+course="Python"
+fees=5000
+print("%s %s fees is %d." %(s, course,fees))
+
+#question-6
+name="Tony Stark"
+salary=1000000
+print(("%s%d")%(name,salary))
+       
