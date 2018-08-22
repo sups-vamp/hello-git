8afd10b2eda0ef55369fe744811d1db78bf01cb1
sups <supriya1880.cse16@chitkara.edu.in>
2018-08-22 21:37:03 +0530

    assign 6

--- assignment6.py
+++ assignment6.py
@@ -1,12 +1,20 @@
 #question-1
-def calculate(rad):
+'''def calculate(rad):
     return(4*3.14*rad*rad)
 rad=int(input("enter radius"))
-print(calculate(rad))
+print(calculate(rad))'''
 
 
 #question-2
-
+def perfect(num):
+    sum=0
+    for j in range(1,num):
+        if num%j==0:
+            sum=sum+j
+    if sum==num:
+        print(num)
+for i in range(1,1000):
+    perfect(i)
 
 
 #question-3
