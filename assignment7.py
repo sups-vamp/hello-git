c57b6c5648238b718cd8e9c6f6b44789df940356
sups <supriya1880.cse16@chitkara.edu.in>
2018-08-23 14:26:02 +0530

    assign7

--- assignment7.py
+++ assignment7.py
@@ -7,3 +7,21 @@ for i in range(1,5):
     value = input("Enter the value: ")
     dict1[key] = value
 print(dict1)
+
+
+#question2
+dict1={}
+dict2={}
+for i in range(1,3):
+    dict2={}
+    name = input("Enter name ")
+    for j in range(1,3):
+              sub=input("enter subject")
+              marks=int(input("enter marks"))
+              dict2[sub]=marks
+    dict1[name]=dict2
+print(dict1)
+student=input("enter the student's name whose marks u want to see")
+print(dict1[student])
+    
+
