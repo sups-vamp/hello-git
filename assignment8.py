45879cff16e255386c1cd7de34104a165c87eb77
sups <supriya1880.cse16@chitkara.edu.in>
2018-08-24 22:48:40 +0530

    assign. 8

---
+++ assignment8.py
@@ -0,0 +1,36 @@
+#ASSIGNMENT - CLASSES AND OBJECTS
+#question-1
+class circle:
+    def __init__(self, r):
+        self.radius = r
+    def getArea(self):
+        return(3.14*self.radius*self.radius)
+    def getCircumference(self):
+       return(2*3.14*self.radius)
+r=int(input("enter radius"))
+c=circle(r)
+print("area is ",c.getArea())
+print("circumference is ",c.getCircumference())
+
+
+#question-2
+
+
+
+#question-3
+
+
+
+#question-4
+
+
+
+#question-5
+
+
+
+#question-6
+
+
+
+#question-7
