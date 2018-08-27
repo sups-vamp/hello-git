434e8005f3a6efd4d4fe12a5fa3a72a88375d78c
sups <supriya1880.cse16@chitkara.edu.in>
2018-08-27 19:52:32 +0530

    assign 9

---
+++ assignment9.py
@@ -0,0 +1,30 @@
+#question-1
+#the code snippet shows ZeroDivisionError
+'''a=3
+if a<4:
+    try:
+        a=a/(a-3)
+    except:
+        print("denominator cannot be zero!")
+print(a)'''
+
+
+#question-2
+#the code snippet shows IndexError
+l=[1,2,3]
+try:
+    print(l[3]) 
+except:
+    print("indexes above 2 aren't acceptable.")
+
+#question-3
+#An exception
+#error
+
+
+#question-4
+#-5.0
+#a/b result in 0
+
+
+#question-5
